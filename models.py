72dbb46962d30b853761369d36b1a99de7dfb3bc
DorianBoel <dorian.boel@free.fr>
2024-06-03 14:21:34 +0200

    - add CommandeArticle table

--- models.py
+++ models.py
@@ -39,6 +39,16 @@ class Commande(Base):
 	NSuivi = Column(String(50), default=None)
 	DateExpedition = Column(Date)
 
+class CommandeArticle(Base):
+	__tablename__ = "commande_article"
+
+	NumCde = Column(Integer, ForeignKey("commande.NumCde"), primary_key=True)
+	CodeArticle = Column(Integer, ForeignKey("article.CodeArticle"), primary_key=True)
+	CodeEmballage = Column(Integer)
+	CodeModele = Column(Integer)
+	Poids = Column(Numeric, default=0.0000)
+	MontantAffranchissement = Column(Float, default=0.0000)
+
 class Utilisateur(Base):
 	__tablename__ = "utilisateur"
 
