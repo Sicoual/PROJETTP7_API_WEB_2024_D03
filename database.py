afc5897d4c4be637377c4aa6c8bf6ad9df7fc844
ilyesbahlagui <ilyesbahlagui@gmail.com>
2024-06-04 10:15:17 +0200

    First commit

---
+++ database.py
@@ -0,0 +1,32 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import  DeclarativeBase
+from os import environ
+# connexion a la base de donnée et déclaration de la base avec sql alchemy
+
+user = environ.get("DB_USER")
+password = environ.get("DB_PASSWORD")
+port = environ.get("DB_PORT")
+database = environ.get("DB_DATABASE_NAME")
+host = environ.get("DB_HOST")
+
+# url de connexion de la base
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://{0}:{1}@{2}:{3}/{4}".format(
+    user,
+    password,
+    host,
+    port,
+    database,
+)
+
+# permet de définir les paramètre de connexion à la base
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# déclaration d'une base qui permet après de créer un modele et de mapper avec sql alchemy
+class Base(DeclarativeBase):
+    pass
+
+# creation d'une session
+# SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+db = SQLAlchemy(model_class=Base)
---
+++ schemas.py
@@ -0,0 +1,9 @@
+from flask_marshmallow import Marshmallow
+
+from models import Client
+
+ma=Marshmallow()
+
+class ClientSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model=Client()
