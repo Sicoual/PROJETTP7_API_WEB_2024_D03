0516ded31d1320d72ea2ecff555db6b12fd43f00
zaid <zaidott71@gmail.com>
2024-06-06 16:25:21 +0200

    documents&tests

--- Test/test_clients.py
+++ Test/test_clients.py
@@ -7,8 +7,15 @@ class TestClients(unittest.TestCase):
          with app.test_client() as client:
             response = client.get('/clients')
             self.assertEqual(response.status_code, 200, "Status code != 200")
-            expected_data = [ { "CodeCli": 1, "Nom": "Dupont", "Prenom": "Jean", "Email": "jean.dupont@example.com", "Adresse": "123 Rue de Paris", "IdCodePostal": 75000, "Genre": "Homme" } ]
-            self.assertEqual(response.json, expected_data, "La réponse ne correspond pas aux données attendues")
+            expected_data = [ { "CodeCli": 1, 
+                               "Nom": "Dupont",
+                               "Prenom": "Jean",
+                               "Email": "jean.dupont@example.com",
+                               "Adresse": "123 Rue de Paris",
+                               "IdCodePostal": 75000,
+                               "Genre": "Homme" } ]
+            self.assertEqual(response.json, expected_data,
+                             "La réponse ne correspond pas aux données attendues")
 
 if __name__ == '__main__':
     unittest.main()
